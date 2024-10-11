1c63e8c06f266047e6dba26606c3ea52ced23e2d
Luis Lima <luisfernando.lima2005@gmail.com>
2024-10-11 15:40:22 -0300

    Estudo sobre módulos

---
+++ ex001-modulos/calc.js
@@ -0,0 +1,8 @@
+var somaFunc = require("./soma")
+var subFunc = require("./sub")
+var multFunc = require("./mult")
+
+
+console.log(somaFunc(4,4))
+console.log(subFunc(4,4))
+console.log(multFunc(100,10))
