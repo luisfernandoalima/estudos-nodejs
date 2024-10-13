c9391de68cf639a3b3eb3a90d2defbd985b74c78
Luis Lima <luisfernando.lima2005@gmail.com>
2024-10-13 10:32:08 -0300

    Utilizando parâmetros nas rotas

--- ex003-express/index.js
+++ ex003-express/index.js
@@ -1,23 +1,27 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
+app.get("/", function (req, res) {
+  res.send("Seja bem-vindo ao meu app");
+  // Página incial
+});
 
-app.get("/", function(req, res){
-    res.send("Seja bem-vindo ao meu app")
-    // Página incial
-})
+app.get("/sobre", function (req, res) {
+  res.send("Minha página sobre");
+  // localhost:8081/sobre
+});
 
-app.get("/sobre", function(req, res){
-    res.send("Minha página sobre")
-    // localhost:8081/sobre
-})
+app.get("/blog", function (req, res) {
+  res.send("Bem-vindo ao meu blog");
+  // localhost:8081/blog
+});
 
-app.get("/blog", function(req, res){
-    res.send("Bem-vindo ao meu blog")
-    // localhost:8081/blog
-})
+app.get("/ola/:nome/:cargo", function (req, res) {
+  res.send(`Olá ${req.params.nome}! Você é ${req.params.cargo}!`);
+  //   req.params -> exibe os parâmetros
+});
 
-app.listen(8081, function(){
-    console.log("Servidor rodando na url http://localhost:8081")
-})
+app.listen(8081, function () {
+  console.log("Servidor rodando na url http://localhost:8081");
+});
 // localhost:8081
