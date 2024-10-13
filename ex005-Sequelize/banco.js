1fd15300b1ffb1872ea13b1020bb0ecc2a65a9f3
Luis Lima <luisfernando.lima2005@gmail.com>
2024-10-13 13:56:05 -0300

    Criando tabelas e inserindo dados no MySQL

--- ex005-Sequelize/banco.js
+++ ex005-Sequelize/banco.js
@@ -4,8 +4,49 @@ const sequelize = new Sequelize('sistemaDeCadastro', 'root', "", {
     dialect: 'mysql'
 })
 
-sequelize.authenticate().then(function(){
-    console.log('Conectado com sucesso!')
-}).catch(function(erro){
-    console.log(`Falha ao se conectar: ${erro}`)
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
 })
+
+const Usuarios = sequelize.define('usuarios',{
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+})
+
+Usuarios.create({
+    nome: 'Luis',
+    sobrenome: 'fernando',
+    idade: 19,
+    email: 'luis@gmail.com'
+})
+
+// Postagem.create({
+//     titulo: 'Olá',
+//     conteudo: 'Lorem'
+// })
+
+
+// Postagem.sync({force: true})
+// Usuarios.sync({force: true})
+
+
+// sequelize.authenticate().then(function(){
+//     console.log('Conectado com sucesso!')
+// }).catch(function(erro){
+//     console.log(`Falha ao se conectar: ${erro}`)
+// })
